70281df9136fceea429512794de0bdfa6a8eeaff
markjconlon <markjconlon@gmail.com>
2017-09-26 14:12:33 -0400

    added addFish method App Component and passed via props through Inventory to AddFish Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,33 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    //getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+  addFish(fish) {
+    //update our state
+    const fishes = {...this.state.fishes}; //takes a copy of existing state
+    //add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // this.state.fishes.fish1 = fish; possible but not advised due to performance
+    //set state
+    this.setState({ fishes: fishes}) //the same as { fishes }
+  }
   render(){
     return(
       <div className="catch-of-the-day">
-        <Header tagline="Fresh Seafood Market"/>
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
