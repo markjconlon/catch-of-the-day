d14c3e9813531921af3e17782992fa5e46db7400
markjconlon <markjconlon@gmail.com>
2017-09-26 14:50:01 -0400

    created loadSamples function in App.js to add sample fish and passed through props down to AddFishForm

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+
     //getinitialState
     this.state = {
       fishes: {},
@@ -23,6 +26,12 @@ class App extends React.Component {
     //set state
     this.setState({ fishes: fishes}) //the same as { fishes }
   }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render(){
     return(
       <div className="catch-of-the-day">
@@ -30,7 +39,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
