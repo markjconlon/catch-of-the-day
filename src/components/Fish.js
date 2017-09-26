98c16b60bc24aeb8957cbb34200d0de959222a74
markjconlon <markjconlon@gmail.com>
2017-09-26 15:08:17 -0400

    added <uL> for list-of-fishes using Object.keys and .map passing the key as the key and the details of each fish

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fish extends React.Component {
+  render(){
+    return (
+      <li className="menu-fish">
+        Fish
+      </li>
+    )
+  }
+}
+
+export default Fish;
