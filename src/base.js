6d0e1fc4f72857ae10aa35d79eb2001fb126511e
markjconlon <markjconlon@gmail.com>
2017-09-28 14:10:42 -0400

    connected to firebase to maintain state by, created base.js in src entered key+domain+url, imported that file into Order.js and added two functions componentWillMount and Unmount

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC7RUnPbYm9mjewPTe33CYHyOBEjxufWB0",
+  authDomain: "catch-of-the-day-mark-conlon.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mark-conlon.firebaseio.com",
+});
+
+export default base;
