05c70c0a10b8528e0c618b296c83c8b8b1e76999
markjconlon <markjconlon@gmail.com>
2017-09-28 11:54:37 -0400

    added the addToOrder function to App.js passing the actual fish index that was clicked via indexwhich is then added via Fish.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     //getinitialState
     this.state = {
@@ -33,6 +34,11 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order: order })
+  }
   render(){
     return(
       <div className="catch-of-the-day">
@@ -41,7 +47,7 @@ class App extends React.Component {
           <ul className="list-of-fishes">
             {
               Object.keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
