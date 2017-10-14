ec9ee56ef6831321e7ecd2a92f4bd4adb361d70c
markjconlon <markjconlon@gmail.com>
2017-10-13 20:16:46 -0400

    added propTypes to Fish.js and Inventory.js

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -46,4 +46,12 @@ class Inventory extends React.Component{
   }
 }
 
+Inventory.propTypes = {
+  fishes: React.PropTypes.object.isRequired,
+  updateFish: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired,
+  addFish: React.PropTypes.func.isRequired,
+  loadSamples: React.PropTypes.object.isRequired,
+}
+
 export default Inventory;
